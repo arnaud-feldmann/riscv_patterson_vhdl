0ae89548b5c3a81e18a2bae2f843d4a6af4dbf48
arnaud-feldmann <arnaud.feldmann@gmail.com>
2024-09-28 17:17:26 +0200

    programme avec toutes les instructions mentionnées qui renvoie le bon résultat

--- program.s
+++ program.s
@@ -11,6 +11,15 @@ sub x5, x5, x6 # x5 = x5-x6 (= 4-2 = 2 puis = 2-2 = 0)
 beq x5, x0, suite # déclenché la deuxième fois
 beq x0, x0, loop
 suite:
+lw x6, 520(x0) # x6 := z* = 4
+lw x0, 520(x0) # doit laisser x0 fixe car x0 est cablé à 0
+lw x7, 524(x0) # x7 := z* = 2
+add x8, x6, x7 # x8 := x6+x7 = 6
+and x9, x10, x8 # x9 := x10 & x8 = 61 & 6 = 4
+or x11, x10, x8 # x11 := x10 | x8 = 61 | 6 = 63
+sub x12, x11, x9 # x12 := x11-x9 = 63-4 = 59
+add x12, x12, x12 # x12 := x12+x12 = 59+59 = 118
+sub x10, x12, x10 # x10 := x12-x10 = 118-61 = 57
 sw x10, 1020(x0) # (1020)* := x10 = 61 (1020 est l'adresse d'entrée/sortie)
 end:
 beq x0, x0, end
